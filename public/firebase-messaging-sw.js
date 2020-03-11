bda3279aba201ab5851cd665a21dcc63dfaa0504
Itay-PC <5647956+itay-sho@users.noreply.github.com>
2020-03-11 02:25:08 +0200

    preparing for another deploy

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,17 @@
 importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-messaging.js');
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCkWIV6dQbYlUa-jU07FZfXGJKC8Kpeb7Q",
+    authDomain: "co-buddies.firebaseapp.com",
+    databaseURL: "https://co-buddies.firebaseio.com",
+    projectId: "co-buddies",
+    storageBucket: "co-buddies.appspot.com",
+    messagingSenderId: "988924831308",
+    appId: "1:988924831308:web:e46ac712f53892b40ab6e9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler((payload) => {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import axios from 'axios'
 import ChatContextProvider from "./context/chat-context";
 import * as firebase from "firebase/app";
@@ -19,7 +18,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 const firebaseConfig = {
     apiKey: "AIzaSyCkWIV6dQbYlUa-jU07FZfXGJKC8Kpeb7Q",
@@ -30,5 +29,4 @@ const firebaseConfig = {
     messagingSenderId: "988924831308",
     appId: "1:988924831308:web:e46ac712f53892b40ab6e9"
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
